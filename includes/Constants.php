9b78050a93aae2af86845ccaa1f6a19f4ed692b0
Dennis Michael <denniskilawilla@gmail.com>
2018-10-24 11:45:28 +0300

    changed the registration title

---
+++ includes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+
+	/*
+	* Created by chief kilawila
+	* website:www.futurebasics.co.tz
+	*/
+
+	 define('DB_HOST', '127.0.0.1:3306');
+	//define('DB_HOST', 'localhost');
+	define('DB_USER', 'dereck');
+	define('DB_PASS', 'rivals2winners');
+	define('DB_NAME', 'agro_vetenarydb');
